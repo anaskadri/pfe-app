880820e0ed9a68fd7bab357efa9ed72480283b6f
mohssine7 <mohssinezernoun2@gmail.com>
2020-09-16 22:28:31 +0100

    création model et migration pour la table preinscription

---
+++ database/migrations/2020_09_16_210209_create_preinscriptions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreinscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preinscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->string('prenom');
+            $table->string('nom');
+            $table->string('civilité');
+            $table->string('email');
+            $table->string('cne');
+            $table->string('cin');
+            $table->unsignedBigInteger('telephone');
+            $table->string('ville');
+            $table->string('file_cv');
+            $table->string('file_cin');
+            $table->string('file_bac');
+            $table->string('file_diplome');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preinscriptions');
+    }
+}
